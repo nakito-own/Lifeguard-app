cc13b74d2374369ebb0853f6963b1ea3dbc2d569
Кирилл <90841328+5544456@users.noreply.github.com>
2024-07-03 20:16:27 +0300

    Custom buttons + textfield

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:lifeguard/widgets/custom_textfield.dart';
 import '../widgets/app_drawer.dart';
+import '../widgets/custom_button.dart';
+import '../widgets/custom_components.dart';
 
 class DetailsScreen extends StatelessWidget {
   @override
@@ -9,18 +12,25 @@ class DetailsScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Details'),
       ),
+
       drawer: AppDrawer(),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+
             Text(arguments['title'], style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
             SizedBox(height: 16),
             Text(arguments['body']),
+            CustomButtonPush(),
+            CustomTextfield(),
+            ButtonExit(),
           ],
         ),
       ),
     );
   }
+
+
 }
---
+++ lib/widgets/custom_components.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+
+class ButtonExit extends StatelessWidget {
+
+  void Exit() {
+    print('Exit');
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          TextButton(
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(Colors.transparent),
+              minimumSize: MaterialStateProperty.all(Size(MediaQuery.of(context).size.width * 0.3, MediaQuery.of(context).size.height * 0.05)),
+            ),
+            onPressed: Exit,
+
+            child: Text('Выход', style: TextStyle(fontSize: 17, fontWeight: FontWeight.w400, color: Color(0xffe91e1e), )),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
